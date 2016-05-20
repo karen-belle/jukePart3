c9cc43f11fed08fa08219f87b709410a6872d169
Karen Abramowitz <karenabramowitz@gmail.com>
2016-05-20 17:01:47 -0400

    email works

--- browser/js/module.js
+++ browser/js/module.js
@@ -2,8 +2,16 @@
 
 var juke = angular.module('juke', ['ui.router']);
 
-juke.run(function ($rootScope) {
-  $rootScope.$on('$stateChangeError', function (event, toState, toParams, fromState, fromParams, error) {
-    console.error('Error transitioning from "' + fromState.name + '" to "' + toState.name + '":', error);
-  });
-});
+juke.run(function($rootScope) {
+    $rootScope.$on('$stateChangeError', function(event, toState, toParams, fromState, fromParams, error) {
+        console.error('Error transitioning from "' + fromState.name + '" to "' + toState.name + '":', error);
+    });
+})
+
+juke.config(function($urlRouterProvider, $locationProvider) {
+    // when there is an empty route, redirect to /index   
+    $urlRouterProvider.when('/', '/albums')
+        .when('/artist/:artist', '/artist/:artist/albums');
+
+    $locationProvider.html5Mode(true);
+})
